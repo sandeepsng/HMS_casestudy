32bb8b2ee7fcf398f0feec3f10b01258b52816dd
RaulS963 <rahulsilver03@gmail.com>
2020-06-27 00:12:31 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import sqlite3
 import datetime
 import os
@@ -21,12 +21,15 @@ def createTable():
 	) ''')
 	conn.commit()
 
+def getCurrentDate():
+	p = datetime.datetime.now()
+	current_date = p.strftime("%Y-%m-%d")
+	print(f"date_today: {current_date}")
+	return current_date
+
 
 def insertIntoTable():
-	p = datetime.datetime.now()
-	date_today = p.strftime("%Y-%m-%d")
-	print(f"date_today: {date_today}")
-	
+	date_today = getCurrentDate()	
 	
 	p_records = [
 		(987412365,100000000,'natsu', 'f-street-01, fiore', 19,date_today,'General',1),
@@ -35,8 +38,6 @@ def insertIntoTable():
 	cur.executemany("INSERT INTO patients VALUES (?,?,?,?,?,?,?,?)" , p_records)
 	print(f"INSERTED {cur.rowcount} rows")
 	conn.commit()
-
-
 
 
 @app.route('/')
@@ -55,9 +56,20 @@ def viewPatientDetails():
 	conn2.close()
 	return render_template("patients.html",patient_details=patient_details,pageTitle="patients details")
 	
-
+@app.route('/addnewpatient',methods=['GET','POST'])
+def addNewPatient():
+	if request.method == 'POST':
+		conn = sqlite3.connect("hospital.db")
+		c = conn.cursor()
+		c.execute("INSERT INTO patients (ws_ssn, ws_pat_name, ws_adrs, ws_age, ws_doj, ws_rtype, ws_status) VALUES (?,?,?,?,?,?,?);", (request.form['p_ssn'],request.form['p_name'],request.form['p_addr'],request.form['p_age'],getCurrentDate(),request.form['p_rtype'],1))
+		conn.commit()
+		c.close()
+		conn.close()
+		return redirect(url_for('viewPatientDetails'))	
+	return render_template("addnewpatients.html",pageTitle="add new patient")
 
 if __name__ == '__main__':
 	createTable()
 	#insertIntoTable()
+	conn.close()
 	app.run(debug=True)
