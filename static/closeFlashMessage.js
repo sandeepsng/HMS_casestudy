94b4eba2f49817d5b39c72677fdc6875f2e76810
sandeepsng <38298766+sandeepsng@users.noreply.github.com>
2020-07-02 17:32:23 +0530

    Delete closeFlashMessage.js

--- static/closeFlashMessage.js
+++
@@ -1,9 +0,0 @@
-function closeFlashMessage() {
-	const msg_box = document.getElementById("flash-message-box");
-	if (msg_box) {
-		setTimeout(function () {
-			document.getElementById("flash-message-box").style.display = "none";
-			console.log("closed!");
-		}, 3000);
-	}
-}
